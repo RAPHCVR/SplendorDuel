6882126c6b82621b67502abc6198a054ea35475c
Mosjil <203sach@gmail.com>
2023-11-07 11:22:04 +0100

    Ajout d'un dossier source avec un makefile, de quoi compiler le projet. A tester sur votre ordi pour voir si ça marche

---
+++ source/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: sacha
+ *
+ * Created on 7 novembre 2023, 11:20
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
