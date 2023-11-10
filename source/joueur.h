b449aa7d5e53c3e7454e4db6d6830e15f6dc438e
Celine Ozkaya <celine.ozkaya@etu.utc.fr>
2023-11-10 12:09:00 +0000

    Update joueur.h

--- source/joueur.h
+++ source/joueur.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <map>
 #include <vector>
+//#include les fichiers qui appelent les autres classes
 
 using namespace std;
 
@@ -18,8 +19,9 @@ private:
     int prestigePoints;
     int nbTokens;
     vector<JewelryCard> cardsJ; //Pas sur de l utilisation de vector
-    vector<CrownCard> cardsC;
-    vector<Token> tokens;
+    vector<JewelryCard> reserve;
+    vector<RoyalCard> cardsC;
+    vector<Token> tokens; 
 
 public:
     string getName() const {return name;};
@@ -29,17 +31,33 @@ public:
     int getTokens() const {return nbTokens;};
     Type getType() const {return type;};
 
-    void reserveCard(JewelryCard card);
-    void buyCard(); //Peut-etre besoin d'une carte ?
-    void removeToken(Token token);
-    void usePrivilege(int privilege);
-    int prestigePerColor();
-    void addCrowns();
-    void addPrestige();
-    void addPrivilege();
-    void addToken(Token token);
-    void addJCard(JewelryCard card); //Pour simplifier buyCard
-    void addCCard(CrownCard card);
+    // ostream 
+
+
+    
+    void removeToken(Token token); // appelé quand on achete une carte ou se fait voler un jeton ou au bout de 10 jetons
+    //int prestigePerColor(); // retourne le total de prestige pour une couleur du joueur
+    void addCrowns(); // compter mes couronnes + prendre une carte couronne si crown = 3 ou 6 (--> appeler )
+    void addPrestige(); // compteur de tous mes prestiges (pour condition de victoire sur 20)
+    void addPrivilege(); // appelee en debut de partie si l'autre commence, si l'autre rempli le plateau, si j'achete une carte avec cette capacité
+    void removePrivilege(); // decrementer le nb de priviliege --> en cas de vol
+    void addJewelryCard(JewelryCard card); //Pour simplifier buyCard  ajout de ma carte achetér au tas de mes cartes
+    void addRoyalCard(RoyalCard card);// ajout d'une carte royale a mon inventaire
+    void addToken(Token token); // ajout d'un jeton a mon inventaire
+
+    // action obligatoires (acheter une carte et/ou prendre des jetons et/ou reserver une carte)
+    void actionAddToken(); // prendre les jetons sur le plateau
+    void actionReserveCard(); // retirer du deck; prendre un or (avec addToken); 
+    void actionBuyCard(); //Peut-etre besoin d'une carte ? prix, utilisation de la capacité... + retirer la carte du jeu (voir si on la fait nous ou dans la classe carte)
+
+    // actions optionnelles (remplir plateau, utiliser un priviliege pour acheter un jeton)
+    void actionFillBoard();
+    void usePrivilege(); // appelé au moment d'acheter un jeton 
+
+    ~Joueur(); // regarder si besoin
+    // interdire copie et affectation 
+    Joueur(string name, Type type) : name(name), type(type), nbCrown(0), prestigePoints(0), nbTokens(0){} // vector?
+
 };
 
 #endif //TEST_JOUEUR_H
