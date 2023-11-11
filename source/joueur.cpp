606475619e228592c3eb78bc9a5726a61a53c842
Celine Ozkaya <celine.ozkaya@etu.utc.fr>
2023-11-11 22:30:21 +0000

    ajout corps methodes

--- source/joueur.cpp
+++ source/joueur.cpp
@@ -59,3 +59,99 @@ void Joueur::removeToken(Token token) {
     vector<Token>::iterator it;
     //it = remove(tokens.begin(), tokens.end(), token); ne marche pas encore
 }
+
+
+// prendre les jetons sur le plateau (demander la rpz des jetons sur le plateau)
+void Player::actionAddToken(){
+        int nb_jetons = 0;
+        Token token1, token2, token3; // token est recopiable
+        int ok; 
+    while(!ok){
+        // affiche les jetons dispo sur le plateau + demander lequel (clique sur le jeton) [griser + non cliquable les cases pas cliquables]
+        // if(nb_jetons == O), else if nb_jeton ==1, 2
+        // a chaque clic --> incrementer nb_jeton + remplir les info du jeton selectionné + demander si ok ==1 ou 0 pour ajout definitif
+
+
+        // 
+    }
+    // ok = 1 --> ajout
+    tab_token_to_add
+    for(int i =0; i< nb_jeton; i++){
+        addToken(tab_token_to_add[i]);
+        // retirer le jeton du plateau (methode plateau)
+    }
+    
+}
+
+// méthode pour vérifier si le joueur a les ressources nécessaires pour acheter une carte    
+// pour vérifier si le joueur a les ressources nécessaires (tokens, bonus)
+// retourne true si le joueur peut acheter la carte, false sinon.
+bool Player::canBuyCard(const JewelryCard &card){}
+
+// méthode pour retirer les ressources nécessaires lorsque le joueur achète une carte
+void Player::spendResources(const JewelryCard &card) {}
+
+void Player::addPrestige(int n, TokenColor color) {
+        prestigePoints += n;
+        switch (color) {
+        case TokenColor::BLEU:
+            blueSummary.addPrestige(n);
+            break;
+        case TokenColor::ROUGE:
+            redSummary.addPrestige(n);
+            break;
+        case TokenColor::VERT:
+            greenSummary.addPrestige(n);
+            break;
+        case TokenColor::BLANC:
+            whiteSummary.addPrestige(n);
+            break;
+        case TokenColor::NOIR:
+            blackSummary.addPrestige(n);
+            break;
+        default:
+        // pas une carte joallerie
+
+    }
+        
+        //cout << "Le joueur " << name << " a gagné " << n << " points de prestige!" << endl;
+}
+
+// On y vérifie que le joueur a les moyens d’acheter la carte sélectionnée 
+// puis retire et place dans le sac les jetons dépensés lors de l’achat, 
+// retire la carte de la pyramide 
+// et ajoute la carte à la liste des cartes possédées par le joueur. 
+
+void Player::actionBuyCard(JewelryCard &card){
+    // Vérifier si le joueur a les ressources nécessaires pour acheter la carte
+        if (canBuyCard(card)) {
+            // Retirer les ressources nécessaires
+            spendResources(card);
+
+            // Ajouter la carte au joueur
+            addJewelryCard(card);
+
+            // Ajouter des points de prestige si la carte a des points associés
+            addPrestige(card.getPrestige(), card.getBonus());
+            // Retirer la carte du plateau de jeu
+            card = Pyramid_Cards::takeCard(card.getLevel(), card.getPosition());
+
+
+            //cout << "La carte a été achetée avec succès par le joueur " << name << "!" << endl;
+        } else {
+            //cout << "Le joueur " << name << " ne peut pas acheter cette carte. Ressources insuffisantes." << endl;
+        }
+    
+
+    // faire un bouton annuler l'achat dans le fichier interface
+    // pour choix du paiement : 
+    // avec Qt faire un bouton + / - limité au nombre de jetons max possédés pour chaque couleur du prix
+    // avec Qt faire un bouton + / - limité au nombre de bonus max possédés pour chaque couleur du prix
+    // au moment du clic sur le bouton acheter, on compte si les jetons+bonus = cout de la carte --> si non on en informe le joueur
+    // si oui : 
+    // on retire dans chaque tableau de jeton, le nb de jeton de la couleur qui a ete depensé
+    //this->tokens[0].pop_back(); // pour chaque jeton utilisé --> dans spendRessources
+
+    
+
+}
