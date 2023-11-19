d8f737b426d82212b94d9514ca8c75c7589f2c79
Celine Ozkaya <celine.ozkaya@etu.utc.fr>
2023-11-19 21:36:59 +0100

    fonction canBuyCard() (classe Player) terminee

--- Rendu2/source/joueur.cpp
+++ Rendu2/source/joueur.cpp
@@ -5,19 +5,32 @@
 #include <map>
 #include <vector>
 #include <unordered_map>
+#include "Jeton.cpp"
+#include "Jeton.h"
+#include "Cards.h"
+#include "Cards.cpp"
 
 using namespace std;
-// premier commit sur la branche Celine
-// second commit
 
-void Joueur::addToken(Token token) {
+vector<int> Player::getBonusSummary() {
+    vector<int> bonus;
+    bonus.push_back(getBlueSummary().getBonusNumber());
+    bonus.push_back(getWhiteSummary().getBonusNumber());
+    bonus.push_back(getGreenSummary().getBonusNumber());
+    bonus.push_back(getBlackSummary().getBonusNumber());
+    bonus.push_back(getRedSummary().getBonusNumber());
+
+    return bonus;
+}
+
+void Player::addToken(Token &token) {
     string color;
     color=token.getColor();
     tokenSummary.at(color)+=1;
     tokens.insert(tokens.begin(),token);
 }
 
-void Joueur::actionReserveCard(){ //Conditions d'activation à penser : moins de 3 cartes en réserve + au moins un or sur plateau
+void Player::actionReserveCard(){ //Conditions d'activation à penser : moins de 3 cartes en réserve + au moins un or sur plateau
     JewelryCard chosenCard;
     Token jetonOr;
     //choix d'une carte joallerie
@@ -34,15 +47,15 @@ void Joueur::actionReserveCard(){ //Conditions d'activation à penser : moins de
     addToken(jetonOr);
 };
 
-void Joueur::addPrivilege() {
+void Player::addPrivilege() {
     privilege+=1;
 }
 
-void Joueur::removePrivilege() {
+void Player::removePrivilege() {
     privilege-=1;
 }
 
-void Joueur::addCrowns() {
+void Player::addCrowns() {
     nbCrown+=1;
     RoyalCard chosenCard;
     if (nbCrown==3 || nbCrown==6){
@@ -55,7 +68,7 @@ void Joueur::addCrowns() {
     }
 }
 
-void Joueur::removeToken(Token token) {
+void Player::removeToken(Token &token) {
     string tokenColor=token.getColor();
     tokenSummary.at(tokenColor)-=1; //retire dans le dico
     vector<Token>::iterator it;
@@ -85,14 +98,6 @@ void Player::actionAddToken(){
     
 }
 
-// méthode pour vérifier si le joueur a les ressources nécessaires pour acheter une carte    
-// pour vérifier si le joueur a les ressources nécessaires (tokens, bonus)
-// retourne true si le joueur peut acheter la carte, false sinon.
-bool Player::canBuyCard(const JewelryCard &card){}
-
-// méthode pour retirer les ressources nécessaires lorsque le joueur achète une carte
-void Player::spendResources(const JewelryCard &card) {}
-
 void Player::addPrestige(int n, TokenColor color) {
         prestigePoints += n;
         switch (color) {
@@ -119,12 +124,55 @@ void Player::addPrestige(int n, TokenColor color) {
         //cout << "Le joueur " << name << " a gagné " << n << " points de prestige!" << endl;
 }
 
+// méthode pour vérifier si le joueur a les ressources nécessaires pour acheter une carte    
+bool Player::canBuyCard(JewelryCard &card){
+    // liste des bonus ordre (bleu,blanc,vert,noir,rouge)
+    vector<int> bonus = getBonusSummary();
+    int goldTokens = getTokenSummary()[TokenColor::OR];
+    unsigned int i = 0;
+    // pour chaque type de jeton, on regarde le cout
+    for(auto elt = getTokenSummary().begin(); elt!=getTokenSummary().end(); elt++){
+        // si cout > tokens du joueur + bonus (= carte trop chere)
+        int costGap = card.getCost()[i] - bonus[i] - elt->second;
+        // achat jeton+bonus pas suffisant
+        if(costGap > 0){
+            // achat de la carte avec or + jetons possible
+            if(goldTokens > costGap){
+                goldTokens = goldTokens - costGap;
+            }
+            // pas assez de gold pour acheter carte
+            else{
+                return false;
+            }
+        }
+        i++;
+    } 
+    return true;   
+}
+    
+
+
+
+
+
+
+
+
+
+// méthode pour retirer les ressources nécessaires lorsque le joueur achète une carte
+void Player::spendResources(int red, int green, int blue, int black, int pearl, int white) {
+
+
+
+}
+
+
 // On y vérifie que le joueur a les moyens d’acheter la carte sélectionnée 
 // puis retire et place dans le sac les jetons dépensés lors de l’achat, 
 // retire la carte de la pyramide 
 // et ajoute la carte à la liste des cartes possédées par le joueur. 
-
-void Player::actionBuyCard(JewelryCard &card){
+// return int pour dire si ca a marché --> modifier si return + pertinent à faire
+int Player::actionBuyCard(JewelryCard &card, int position){
     // Vérifier si le joueur a les ressources nécessaires pour acheter la carte
         if (canBuyCard(card)) {
             // Retirer les ressources nécessaires
@@ -135,16 +183,18 @@ void Player::actionBuyCard(JewelryCard &card){
 
             // Ajouter des points de prestige si la carte a des points associés
             addPrestige(card.getPrestige(), card.getBonus());
+
             // Retirer la carte du plateau de jeu
-            card = Pyramid_Cards::takeCard(card.getLevel(), card.getPosition());
-
-
+            card = Pyramid_Cards::takeCard(card.getLevel(), position);
             //cout << "La carte a été achetée avec succès par le joueur " << name << "!" << endl;
-        } else {
+            return 1;
+        } 
+        else {
             //cout << "Le joueur " << name << " ne peut pas acheter cette carte. Ressources insuffisantes." << endl;
+            return 0;
         }
     
-
+    // idees qt
     // faire un bouton annuler l'achat dans le fichier interface
     // pour choix du paiement : 
     // avec Qt faire un bouton + / - limité au nombre de jetons max possédés pour chaque couleur du prix
@@ -154,6 +204,5 @@ void Player::actionBuyCard(JewelryCard &card){
     // on retire dans chaque tableau de jeton, le nb de jeton de la couleur qui a ete depensé
     //this->tokens[0].pop_back(); // pour chaque jeton utilisé --> dans spendRessources
 
-    
 
 }
