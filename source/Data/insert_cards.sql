9716ab03705457fd560d5effad5bf4d58e3c4586
rtheolier <remy.theolier@etu.utc.fr>
2023-11-21 11:19:37 +0100

    Ajout des fichiers de la BdD

---
+++ source/Data/insert_cards.sql
@@ -0,0 +1,25 @@
+INSERT INTO CardsTable
+  ( categorie, capacity, prestige )
+VALUES
+  ( 'Royal', 'Steal', 2 );
+
+INSERT INTO CardsTable
+  ( categorie, capacity, prestige )
+VALUES
+  ( 'Royal', 'Privilege', 2 );
+
+INSERT INTO CardsTable
+  ( categorie, prestige )
+VALUES
+  ( 'Royal', 3 );
+
+INSERT INTO CardsTable
+  ( categorie, capacity, prestige )
+VALUES
+  ( 'Royal', 'Replay', 2 );
+
+INSERT INTO CardsTable
+  ( categorie, prestige, crown, cost_w, cost_v, cost_n, cost_p, color, lvl )
+VALUES
+  ( 'Jewelry', 3, 2, 3, 3, 5, 1, 'blue', 3 );
+
