4eb636e3c0235204bf8ded4ea8b2a5a5dc7b4972
Celine Ozkaya <celine.ozkaya@etu.utc.fr>
2023-12-17 00:38:53 +0100

    AiStrategy::choseOptionalActions(), AiStrategy::random() done

--- source/strategy.cpp
+++ source/strategy.cpp
@@ -1,6 +1,7 @@
 #include "Jeton.h"
 #include "Cards.h"
 #include "joueur.h"
+#include "strategy.h"
 
 #include <algorithm>
 #include <iostream>
@@ -9,3 +10,39 @@
 #include <unordered_map>
 
 
+int AiStrategy::random(int min, int max){
+    int random;
+    // générateur de nombres aléatoires avec le temps actuel
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    // nombre aléatoire dans l'intervalle [min, max]
+    random = std::rand() % (max - min + 1) + min;
+    return random;
+}
+
+
+std::vector<Controller::OptionalActions> AiStrategy::choseOptionalActions(){
+    std::vector<Controller::OptionalActions> optionalActions;
+    // combien d'action optionnelles (entre 0 et 2)
+    int nbOptionalAction = random(0,2);
+
+    // lesquelles parmis 
+    // utiliser privilege = prendre jeton (pas or) / remplir plateau + prendre un privilege
+
+    // on ne realise pas d'action optionnelle
+    if (nbOptionalAction == 0){
+        optionalActions.push_back(OptionalActions::Empty);
+    }
+    //on realise une action optionnelle
+    else if(nbOptionalAction==1){
+        // choisir laquelle
+        int whichOpAction = random(0,1);
+        whichOpAction == 0? optionalActions.push_back(OptionalActions::UsePrivileges) : optionalActions.push_back(OptionalActions::FillBoard);
+    }
+    else{
+        // faire les deux actions
+        optionalActions.push_back(OptionalActions::UsePrivileges);
+        optionalActions.push_back(OptionalActions::FillBoard);
+    }
+    return optionalActions;
+}
---
+++ source/strategy.h
@@ -0,0 +1,51 @@
+#ifndef STRATEGY_H
+#define STRATEGY_H
+
+#include "Jeton.h"
+#include "Cards.h"
+#include "joueur.h"
+#include "Controller.h"
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <unordered_map>
+
+/*
+rappels
+actions obligatoire : 1 seule --> 3 jetons / resa + or / acheter carte
+actions optionnelles : 0, 1 ou 2 --> utiliser privilege = prendre jeton (pas or) / remplir plateau + prendre un privilege
+*/
+
+/*
+CHOIX :
+- nb action optionnels + laquelle
+- quelle action obligatoire
+*/
+
+
+class Strategy{
+public:
+    virtual ~Strategy() = default;
+    virtual int random(int min, int max)=0 ;
+    virtual vector<OptionalActions> choseOptionalActions()=0;
+
+};
+
+
+class HumanStrategy : public Strategy{
+public:
+    virtual vector<OptionalActions> choseOptionalActions();
+
+};
+
+
+class AiStrategy : public Strategy{
+public:
+    virtual int random(int min, int max) override;
+    virtual vector<OptionalActions> choseOptionalActions();
+
+
+};
+
+#endif //STRATEGY_H
