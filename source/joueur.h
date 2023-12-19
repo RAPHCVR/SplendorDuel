eeb945f51fe5a73c51330c9d07e8b373ca155ba4
Celine Ozkaya <celine.ozkaya@etu.utc.fr>
2023-12-19 11:32:11 +0100

    attribut strategy dans Player

--- source/joueur.h
+++ source/joueur.h
@@ -2,6 +2,7 @@
 #define TEST_JOUEUR_H
 #include "Jeton.h"
 #include "Cards.h"
+#include "strategy.h"
 
 #include <iostream>
 #include <map>
@@ -37,7 +38,8 @@ private:
     SummaryCard redSummary;
     SummaryCard whiteSummary;
     SummaryCard blackSummary;
-
+    
+    Strategy* strategy;
 
 public:
     std::string getName() const {return name;};
@@ -61,7 +63,7 @@ public:
     std::vector<JewelryCard*>& getJewelryCards(){ return jewelryCards;}
     std::vector<RoyalCard*>& getRoyalCards(){ return royalCards;}
     std::vector<JewelryCard*>& getReserve(){ return reserve;}
-
+    Strategy& getStrategy(){return strategy;}
     // ostream
 
 
